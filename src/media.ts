5d2e8e020d251aadb42a815943b0bf9f52ce3a8a
pengbo <pbstar@sina.cn>
2025-02-25 14:10:19 +0800

    v1.0.0-beta.4：添加isAudio配置，优化视频清晰度，修复已知bug

--- src/media.ts
+++ src/media.ts
@@ -32,8 +32,21 @@ const createProcessedStream = (media: any, config: any, err: any) => {
 };
 const drawVideoFrame = (v: any, media: any, config: any, err: any) => {
   if (!media.canvasCtx || !media.video) return;
+  const scaleRatio = Math.max(
+    media.video.clientWidth / v.videoWidth,
+    media.video.clientHeight / v.videoHeight
+  );
 
-  media.canvasCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight);
+  // 根据缩放比例计算绘制时的宽度和高度
+  const drawWidth = v.videoWidth * scaleRatio;
+  const drawHeight = v.videoHeight * scaleRatio;
+
+  // 计算图像在Canvas上的目标位置，使其居中
+  const x = (media.video.clientWidth - drawWidth) / 2;
+  const y = (media.video.clientHeight - drawHeight) / 2;
+
+  media.canvasCtx.drawImage(v, x, y, drawWidth, drawHeight);
+
   drawWatermark(media, config, err);
   media.animationFrameId = requestAnimationFrame(() =>
     drawVideoFrame(v, media, config, err)
@@ -98,26 +111,19 @@ export const setupCamera = async (media: any, config: any, err: any) => {
     media.mediaStream = await navigator.mediaDevices.getUserMedia({
       video: {
         facingMode,
-        width: media.video.clientWidth,
-        height: media.video.clientHeight,
       },
       audio: config.isAudio,
     });
-
     // 初始化Canvas
     media.canvasCtx = canvas.getContext("2d", {
       alpha: false, // 关闭透明度提升渲染性能
       willReadFrequently: false, // 关闭频繁读取提升渲染性能
     });
-
     // 处理水印图片
     if (config.watermark.visible && config.watermark.image) {
       const img = new Image();
       const dpr = window.devicePixelRatio || 1;
-      img.src =
-        config.watermark.image.url +
-        "?r=" +
-        Math.random().toString(36).substr(2);
+      img.src = config.watermark.image.url + "?r=" + new Date().getTime();
       img.width = (config.watermark.image.width || 100) * dpr;
       img.height = (config.watermark.image.height || 100) * dpr;
       img.crossOrigin = "Anonymous";
